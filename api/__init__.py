27619977adb0ac5acc66d2c3e896e6be80748838
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-06-17 14:51:21 +0200

    Removed etag comparison

--- api/__init__.py
+++ api/__init__.py
@@ -39,57 +39,6 @@ if _security_configuration.scope_string_value is None:
     _security_configuration.scope_string_value = service_scope.value
 
 
-# %% Middlewares
-@service.middleware("http")
-async def etag_comparison(request: fastapi.Request, call_next):
-    """
-    A middleware which will hash the request path and all parameters transferred to this
-    microservice and will check if the hash matches the one of the ETag which was sent to the
-    microservice. Furthermore, it will take the generated hash and append it to the response to
-    allow caching
-
-    :param request: The incoming request
-    :type request: fastapi.Request
-    :param call_next: The next call after this middleware
-    :type call_next: callable
-    :return: The result of the next call after this middle ware
-    :rtype: fastapi.Response
-    """
-    # Access all parameters used for creating the hash
-    path = request.url.path
-    query_parameter = dict(request.query_params)
-    # Now iterate through all query parameters and make sure they are sorted if they are lists
-    for key, value in dict(query_parameter).items():
-        # Now check if the value is a list
-        if isinstance(value, list):
-            query_parameter[key] = sorted(value)
-
-    query_dict = {
-        "request_path": path,
-        "request_query_parameter": query_parameter,
-    }
-    query_data = orjson.dumps(query_dict, option=orjson.OPT_SORT_KEYS)
-    # Now create a hashsum of the query data
-    query_hash = hashlib.sha3_256(query_data).hexdigest()
-    # Now access the headers of the request and check for the If-None-Match Header
-    if_none_match_value = request.headers.get("If-None-Match")
-    if_modified_since_value = request.headers.get("If-Modified-Since")
-    if if_modified_since_value is None:
-        if_modified_since_value = datetime.datetime.fromtimestamp(0, tz=pytz.UTC)
-    else:
-        if_modified_since_value = email.utils.parsedate_to_datetime(if_modified_since_value)
-    # Get the last update of the schema from which the service gets its data from
-    last_database_modification = tools.get_last_schema_update("water_usage", database.engine)
-    data_changed = if_modified_since_value < last_database_modification
-    if query_hash == if_none_match_value and not data_changed:
-        return fastapi.Response(status_code=304, headers={"ETag": f"{query_hash}"})
-    else:
-        response: fastapi.Response = await call_next(request)
-        response.headers.append("ETag", f"{query_hash}")
-        response.headers.append("Last-Modified", email.utils.format_datetime(last_database_modification))
-        return response
-
-
 # %% Routes
 @service.get("/")
 async def get(
@@ -106,7 +55,7 @@ async def get(
         ],
         database.tables.usages.c.consumer == consumer,
     ).order_by(database.tables.usages.c.year)
-    query_result = database.engine.execute(query).all()
+    query_result = database.engine.execute(query).fetchall()
     if len(query_result) == 0:
         return fastapi.Response(status_code=204)
     return query_result
