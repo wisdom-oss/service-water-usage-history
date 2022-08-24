185c40e07910909ba968a1260e14a31e0e4cff29
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-24 13:13:46 +0200

    Moved vars to own package to make them available globally

--- src/vars/vars.go
+++ src/vars/vars.go
@@ -1,22 +1,26 @@
 // This file contains all globally used variables and their default values
-package main
+package vars
 
 import (
-	"microservice/structs"
 	"os"
+
+	"microservice/structs"
 )
+
+// TODO: Change me
+const ServiceName = "change-me"
 
 var (
 	// The host on which the API gateway runs and this service shall be registered on
-	apiGatewayHost = os.Getenv("CONFIG_API_GATEWAY_HOST")
+	ApiGatewayHost = os.Getenv("CONFIG_API_GATEWAY_HOST")
 	// The administration port of the api gateway on the host
-	apiGatewayAdminPort = os.Getenv("CONFIG_API_GATEWAY_ADMIN_PORT")
+	ApiGatewayAdminPort = os.Getenv("CONFIG_API_GATEWAY_ADMIN_PORT")
 	// The path on which the service shall be reachable
-	apiGatewayServicePath = os.Getenv("CONFIG_API_GATEWAY_SERVICE_PATH")
+	ApiGatewayServicePath = os.Getenv("CONFIG_API_GATEWAY_SERVICE_PATH")
 	// The http port on which the service will listen for new requests
-	httpListenPort = os.Getenv("CONFIG_HTTP_LISTEN_PORT")
+	HttpListenPort = os.Getenv("CONFIG_HTTP_LISTEN_PORT")
 	// Indicator if a health check shall be executed instead of the main() function
-	executeHealthcheck = false
+	ExecuteHealthcheck = false
 	// The scope the service was configured with
-	scope *structs.ScopeInformation
+	Scope *structs.ScopeInformation
 )
