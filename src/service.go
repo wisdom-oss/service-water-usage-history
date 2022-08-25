7edddb1669ca0ad5b1350a59a328994298df4348
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-25 09:36:54 +0200

    Added healthcheck execution and http server startup to the service.go file

--- src/service.go
+++ src/service.go
@@ -1,8 +1,32 @@
 package main
 
-// TODO: Change the service name
-const serviceName = "change-me"
+import (
+	"net/http"
+	"os"
 
+	"microservice/handlers"
+	"microservice/vars"
+)
+
+/*
+This function is used to set up the http server for the microservice
+*/
 func main() {
+	if vars.ExecuteHealthcheck {
+		response, err := http.Get("http://localhost:" + vars.HttpListenPort + "/ping")
+		if err != nil {
+			os.Exit(1)
+		}
+		if response.StatusCode != 204 {
+			os.Exit(1)
+		}
+		return
+	}
 
+	// Set up the HTTP server
+	http.Handle("/ping", http.HandlerFunc(handlers.PingHandler))
+	// Protect the request handler with authorization
+	http.Handle("/", handlers.AuthorizationCheck(http.HandlerFunc(handlers.RequestHandler)))
+	// Start the http server
+	http.ListenAndServe(":"+vars.HttpListenPort, nil)
 }
