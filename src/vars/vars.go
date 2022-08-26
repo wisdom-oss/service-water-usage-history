f65bf245c284c5c3dd58d0a03d610ca3b14675c1
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-26 11:38:48 +0200

    Extended the documentation of the global variables

--- src/vars/vars.go
+++ src/vars/vars.go
@@ -11,21 +11,54 @@ import (
 const ServiceName = "template-service"
 
 var (
-	// The host on which the API gateway runs and this service shall be registered on
+	/*
+		ApiGatewayHost
+
+		The host on which the api gateway resides. The api gateway used in the project is the Kong API Gateway
+	*/
 	ApiGatewayHost string
-	// The administration port of the api gateway on the host
+	/*
+		ApiGatewayAdminPort
+
+		The port on which the admin api of the gateway listens for incoming requests.
+	*/
 	ApiGatewayAdminPort string
-	// The path on which the service shall be reachable
+	/*
+		ApiGatewayServicePath
+
+		The path under which the service is reachable. The path needs to start with a leading slash "/"
+	*/
 	ApiGatewayServicePath string
-	// The http port on which the service will listen for new requests
+	/*
+		HttpListenPort
+
+		The port on which the internally used http server listens for new connections.
+
+		Default value: 8000
+	*/
 	HttpListenPort string
-	// Indicator if a health check shall be executed instead of the main() function
+	/*
+		ExecuteHealthcheck
+
+		An indicator which sets the behaviour of the main function of the microservice.
+		If it is set to true the service will try to access itself after executing all init() steps.
+
+		Default value: false
+	*/
 	ExecuteHealthcheck bool
-	// ScopeConfigFilePath
-	// The path to the location where the scope configuration is stored.
-	// Default: /microservice/res/scope.json
+	/*
+		ScopeConfigFilePath
+
+		The path under which the scope configuration for this service is stored.
+
+		Default value: /microservice/res/scope.json
+	*/
 	ScopeConfigFilePath string
-	// The scope the service was configured with
+	/*
+		Scope
+
+		The parsed scope configuration file which is used by the Authorizer
+	*/
 	Scope *structs.ScopeInformation
 	/*
 		PostgresHost
