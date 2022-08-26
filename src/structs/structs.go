9be22fb032cb7f5ad8496b51ace24885c13180c9
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-26 11:40:05 +0200

    Removed some of the structs which have falsely been copied into the branch

--- src/structs/structs.go
+++ src/structs/structs.go
@@ -1,7 +1,5 @@
 package structs
 
-import geoJson "github.com/paulmach/go.geojson"
-
 type ScopeInformation struct {
 	JSONSchema       string `json:"$schema"`
 	ScopeName        string `json:"name"`
@@ -15,29 +13,4 @@ type RequestError struct {
 	ErrorCode        string `json:"error"`
 	ErrorTitle       string `json:"errorName"`
 	ErrorDescription string `json:"errorDescription"`
-}
-
-/*
-Consumer
-
-The consumer holds the following information:
-	- UUID: The internal and external id for the consumer
-	- Name: The name of the consumer
-	- Location: A GeoJSON entity containing the location of the consumer
-*/
-type Consumer struct {
-	UUID     string           `json:"id"`
-	Name     string           `json:"name"`
-	Location geoJson.Geometry `json:"location"`
-}
-
-/*
-IncomingConsumerData
-
-This struct holds the data for creating a new consumer or updating one
-*/
-type IncomingConsumerData struct {
-	Name      string  `json:"name"`
-	Latitude  float64 `json:"lat"`
-	Longitude float64 `json:"long"`
 }
