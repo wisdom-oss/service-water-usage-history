a1c32c272df2af3e61816b13f51989de85ba394a
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-10-27 13:48:44 +0200

    Moving variables in code for clearer usage recognition

--- src/vars/vars.go
+++ src/vars/vars.go
@@ -11,30 +11,14 @@ import (
 // TODO: Change the service name and remove the TODO comment
 const ServiceName = "template-service"
 
+// ===== Required Setting Variables =====
 var (
-
 	// APIGatewayHost contains the IP address or hostname of the used Kong API Gateway
 	APIGatewayHost string
-
-	// APIGatewayPort contains the port on which the admin api of the Kong API Gateway listens
-	APIGatewayPort int
 
 	// ServiceRoutePath is the path under which the instance of the microservice shall be reachable via the Kong API
 	// Gateway
 	ServiceRoutePath string
-
-	// ListenPort is the port this microservice will listen on. It defaults to 8000
-	ListenPort string
-
-	// ExecuteHealthcheck is an indicator for the microservice if the service shall execute a healthcheck.
-	//You can trigger a health check by starting the executable with -healthcheck
-	ExecuteHealthcheck bool
-
-	// ScopeConfigurationPath specifies from where the service should read the configuration of the needed access scope
-	ScopeConfigurationPath string
-
-	// ScopeConfiguration containing the information about the scope needed to access this service
-	ScopeConfiguration *structs.ScopeInformation
 
 	// DatabaseHost specifies the host on which the postgres database runs on
 	DatabaseHost string
@@ -44,10 +28,31 @@ var (
 
 	// DatabaseUserPassword is the password of the user accessing the database
 	DatabaseUserPassword string
+)
+
+// ===== Optional Setting Variables =====
+var (
+	// ListenPort is the port this microservice will listen on. It defaults to 8000
+	ListenPort int = 8000
 
 	// DatabasePort specifies on which port the database used listens on
-	DatabasePort string
+	DatabasePort int = 5432
 
-	// PostgresConnection is as connection shared throughout the service
-	PostgresConnection *sql.DB
+	// ScopeConfigurationPath specifies from where the service should read the configuration of the needed access scope
+	ScopeConfigurationPath string = "/res/scope.json"
+
+	// APIGatewayPort contains the port on which the admin api of the Kong API Gateway listens
+	APIGatewayPort int = 8001
 )
+
+// ===== Globally used variables =====
+
+// PostgresConnection is as connection shared throughout the service
+var PostgresConnection *sql.DB
+
+// ScopeConfiguration containing the information about the scope needed to access this service
+var ScopeConfiguration *structs.ScopeInformation
+
+// ExecuteHealthcheck is an indicator for the microservice if the service shall execute a healthcheck.
+//You can trigger a health check by starting the executable with -healthcheck
+var ExecuteHealthcheck bool
