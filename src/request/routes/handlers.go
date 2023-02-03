a8ddf0e086b4b7f034d91dca4da3b0b324eaf8b1
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-03 10:39:35 +0100

    chore: refactoring healthcheck

--- src/request/routes/handlers.go
+++ src/request/routes/handlers.go
@@ -8,27 +8,11 @@ import (
 )
 
 /*
-PingHandler
-
-This handler is used to test if the service is able to ping itself. This is done to run a healthcheck on the container
-*/
-func PingHandler(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNoContent)
-}
-
-/*
 BasicHandler
 
 This handler shows how a basic handler works and how to send back a message
 */
 func BasicHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	logger := log.WithFields(
-		log.Fields{
-			"middleware": true,
-			"title":      "BasicHandler",
-		},
-	)
-	logger.WithField("request", request).Info("Got new request")
 	_, err := responseWriter.Write([]byte("Hello World"))
 	if err != nil {
 		log.WithError(err).Error("Unable to send response to the client")
