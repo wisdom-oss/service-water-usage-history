7d8811bd323f9c954ddd62c752e373211dd3f9eb
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-03 10:50:56 +0100

    chore: replaced `gorilla/mux` with `go-chi/chi` due to deprecation

--- src/service.go
+++ src/service.go
@@ -3,16 +3,19 @@ package main
 import (
 	context2 "context"
 	"fmt"
-	"microservice/request/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	gateway "github.com/wisdom-oss/golang-kong-access"
+	middleware2 "microservice/request/middleware"
+	"microservice/request/routes"
+	"microservice/utils"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
-	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 
-	"microservice/request/routes"
 	"microservice/vars"
 )
 
@@ -33,10 +36,14 @@ func main() {
 	}
 
 	// Set up the routing of the different functions
-	router := mux.NewRouter()
-	router.Use(middleware.AuthorizationCheck)
-	router.HandleFunc("/ping", routes.PingHandler)
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware2.AuthorizationCheck)
+	router.Use(middleware2.AdditionalResponseHeaders)
 	router.HandleFunc("/", routes.BasicHandler)
+	router.HandleFunc("/healthcheck", routes.HealthCheck)
 
 	// Configure the HTTP server
 	server := &http.Server{
@@ -62,19 +69,34 @@ func main() {
 	// Block further code execution until the shutdown signal was received
 	<-cancelSignal
 
+	log.Info("Shutting down the microservice...")
+
+	log.Info("Closing the database connection")
+	dbCloseErr := vars.PostgresConnection.Close()
+	if dbCloseErr != nil {
+		log.WithError(dbCloseErr).Fatal("An error occurred while closing the connection to the database")
+	}
+	localIPAddress, _ := utils.LocalIPv4Address()
+	targetAddress := fmt.Sprintf("%s:%d", localIPAddress, vars.ListenPort)
+
+	success, err := gateway.DeleteUpstreamTarget(targetAddress, vars.ServiceName)
+	if err != nil {
+		log.WithError(err).Fatal("unable to deregister the service instance")
+	}
+
+	if success {
+		log.Info("deleted target from the upstream")
+	}
+
 	context, cancel := context2.WithTimeout(context2.Background(), time.Second*15)
 	defer cancel()
 
 	go func() {
-		err := server.Shutdown(context)
+
+		err = server.Shutdown(context)
 		if err != nil {
 			log.WithError(err).Fatal("An error occurred while stopping the http server")
 		}
-		log.Info("Closing the database connection")
-		dbCloseErr := vars.PostgresConnection.Close()
-		if dbCloseErr != nil {
-			log.WithError(err).Fatal("An error occurred while closing the connection to the database")
-		}
 	}()
-	log.Info("Shutting down the microservice...")
+
 }
