31bf24658bc67a48b9c77c60d52bee35c9084217
Jan Eike Suchard <jan@suchard.cloud>
2023-05-17 09:58:05 +0200

    🚧 adding microservice template

---
+++ src/request/error/functions.go
@@ -0,0 +1,66 @@
+package requestErrors
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/wisdom-oss/microservice-utils"
+	"microservice/structs"
+	"microservice/vars"
+	"microservice/vars/globals"
+	"net/http"
+)
+
+// RequestErrors contains all request errors which were read at the starup
+// of the service
+var RequestErrors map[string]structs.RequestError = make(map[string]structs.RequestError)
+
+func GetRequestError(errorCode string) (*structs.ErrorResponse, error) {
+	if !wisdomUtils.MapHasKey(RequestErrors, errorCode) {
+		return nil, vars.ErrHttpErrorNotFound
+	}
+	// since the key exists in the request errors map get the request error
+	requestError := RequestErrors[errorCode]
+	// now get the text for the http error code
+	httpText := http.StatusText(requestError.HttpCode)
+	if httpText == "" {
+		return &structs.ErrorResponse{
+			HttpStatus:       requestError.HttpCode,
+			HttpError:        "Unknown HTTP Code",
+			ErrorCode:        fmt.Sprintf("%s.%s", globals.ServiceName, errorCode),
+			ErrorTitle:       requestError.ErrorTitle,
+			ErrorDescription: requestError.ErrorDescription,
+		}, nil
+	} else {
+		return &structs.ErrorResponse{
+			HttpStatus:       requestError.HttpCode,
+			HttpError:        httpText,
+			ErrorCode:        errorCode,
+			ErrorTitle:       requestError.ErrorTitle,
+			ErrorDescription: requestError.ErrorDescription,
+		}, nil
+	}
+}
+
+// WrapInternalError wraps an internal error and adds the error to the response
+func WrapInternalError(err error) (*structs.ErrorResponse, error) {
+	response, err := GetRequestError("INTERNAL_ERROR")
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to create internal error")
+	}
+	response.ErrorDescription += fmt.Sprintf("%s", err.Error())
+	return response, nil
+}
+
+// SendError takes the request error and sends it back to the client
+func SendError(errorResponse *structs.ErrorResponse, w http.ResponseWriter) {
+	// set the content-type header to json
+	w.Header().Set("Content-Type", "text/json")
+	// now send the http code to the client
+	w.WriteHeader(errorResponse.HttpStatus)
+	// now write out the response
+	err := json.NewEncoder(w).Encode(errorResponse)
+	if err != nil {
+		globals.HttpLogger.Err(err).Msg("unable to send error response")
+	}
+}
---
+++ src/structs/structs.go
@@ -0,0 +1,18 @@
+package structs
+
+// ScopeInformation contains the information about the scope for this service
+type ScopeInformation struct {
+	JSONSchema       string `json:"$schema"`
+	ScopeName        string `json:"name"`
+	ScopeDescription string `json:"description"`
+	ScopeValue       string `json:"scopeStringValue"`
+}
+
+// ErrorResponse contains all information about an error which shall be sent back to the client
+type ErrorResponse struct {
+	HttpStatus       int    `json:"httpCode"`
+	HttpError        string `json:"httpError"`
+	ErrorCode        string `json:"error"`
+	ErrorTitle       string `json:"errorName"`
+	ErrorDescription string `json:"errorDescription"`
+}
