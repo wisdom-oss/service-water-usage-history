4f82df61bfe013984139a457b131f208fddcbe4f
Jan Eike Suchard <jan@suchard.cloud>
2023-05-19 09:32:49 +0200

    ♻️ reformatting, reordering and optimizing queries

--- res/queries.sql
+++ res/queries.sql
@@ -1,65 +1,158 @@
--- Insert a new consumer type into the database
--- name: add-consumer-type
-INSERT INTO water_usage.usage_types (name, description, external_identifier)
-VALUES ($1, $2, $3)
-RETURNING id;
+/* Queries related to the consumers */
 
--- Update a current consumer type's name
+-- Get a consumer by it's uuid
+-- name: get-consumer-by-uuid
+SELECT
+    id,
+    name,
+    ST_AsGeoJSON(location) as location,
+    usage_type,
+    additional_properties
+FROM
+    consumers.consumers
+WHERE
+    id = $1::uuid;
+
+
+/* Queries related to the usage types */
+-- Get a usage type by it's UUID
+-- name: get-single-usage-type-by-id
+SELECT
+    id,
+    name,
+    description,
+    external_identifier
+FROM
+    water_usage.usage_types
+WHERE
+    id = $1::uuid;
+
+-- Get a usage type by it's external identifier
+-- name: get-single-usage-type-by-external-identifier
+SELECT
+    id,
+    name,
+    description,
+    external_identifier
+FROM
+    water_usage.usage_types
+WHERE
+    external_identifier = $1;
+
+-- Update a current usage type's name
 -- name: update-consumer-type-name
-UPDATE water_usage.usage_types
-SET name = $1
-WHERE id = $2;
+UPDATE
+    water_usage.usage_types
+SET
+    name = $1
+WHERE
+    id = $2;
 
--- Update a current consumer type's description
--- name: update-consumer-type-description
-UPDATE water_usage.usage_types
-SET description = $1
-WHERE id = $2;
+-- Update a current usage type's description
+-- name: update-usage-type-description
+UPDATE
+    water_usage.usage_types
+SET
+    description = $1
+WHERE
+    id = $2;
 
--- Update a current consumer type's external identifier
--- name: update-consumer-type-identifier
-UPDATE water_usage.usage_types
-SET external_identifier = $1
-WHERE id = $2;
+-- Update a current usage type's external identifier
+-- name: update-usage-type-identifier
+UPDATE
+    water_usage.usage_types
+SET
+    external_identifier = $1
+WHERE
+    id = $2;
 
--- Delete a consumer type
--- name: delete-consumer-type
-DELETE FROM water_usage.usage_types
-WHERE id = $1;
+-- Insert a new usage type into the database
+-- name: add-usage-type
+INSERT INTO
+    water_usage.usage_types (name, description, external_identifier)
+VALUES
+    ($1, $2, $3)
+RETURNING
+    id;
 
+-- Delete a usage type
+-- name: delete-usage-type
+DELETE FROM
+    water_usage.usage_types
+WHERE
+    id = $1;
+
+/* Queries related to usage records */
 -- Get all water usage records
 -- name: get-all-usages
-SELECT *
-FROM water_usage.usages;
+SELECT
+    id,
+    municipality,
+    date,
+    consumer,
+    usage_type,
+    created_at,
+    amount
+FROM
+    water_usage.usages;
 
 -- Get water usages by consumer
 -- name: get-consumers-usages
-SELECT *
-FROM water_usage.usages
-WHERE consumer = $1::uuid;
+SELECT
+    id,
+    municipality,
+    date,
+    consumer,
+    usage_type,
+    created_at,
+    amount
+FROM
+    water_usage.usages
+WHERE
+    consumer = $1::uuid;
 
 -- Get water usages by type
 -- name: get-type-based-usages
-SELECT *
-FROM water_usage.usages
-WHERE usage_type = (
-    SELECT id
-    FROM water_usage.usage_types
-    WHERE external_identifier = $1
-);
+SELECT
+    id,
+    municipality,
+    date,
+    consumer,
+    usage_type,
+    created_at,
+    amount
+FROM
+    water_usage.usages
+WHERE
+    usage_type = $1::uuid;
 
 -- Get water usages by time range
 -- name: get-usages-between-times
-SELECT *
-FROM water_usage.usages
-WHERE date < $1 AND date > $2;
+SELECT
+    id,
+    municipality,
+    date,
+    consumer,
+    usage_type,
+    created_at,
+    amount
+FROM
+    water_usage.usages
+WHERE
+    date < $1
+AND
+    date > $2;
 
 -- Add a new usage record
 -- name: add-usage-record
-INSERT INTO water_usage.usages(municipality, date, consumer, usage_type, amount)
-VALUES ($1, $2, $3, $4, $5);
+INSERT INTO
+    water_usage.usages(municipality, date, consumer, usage_type, amount)
+VALUES
+    ($1, $2, $3, $4, $5);
 
 -- Delete a usage record
 -- name: delete-usage-record
-DELETE FROM water_usage.usages
-WHERE id = $1;
+DELETE FROM
+    water_usage.usages
+WHERE
+    id = $1;
