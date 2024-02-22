643773f3a866e9577c8b565b2a9d5c3a710dd2ca
Jan Eike Suchard <jan@suchard.cloud>
2024-02-22 14:05:39 +0100

    fixing interface typing

--- routes/templates.go
+++ routes/templates.go
@@ -14,8 +14,8 @@ func BasicHandler(w http.ResponseWriter, r *http.Request) {
 
 func BasicWithErrorHandling(w http.ResponseWriter, r *http.Request) {
 	// access the error handlers
-	errorHandler := r.Context().Value(wisdomMiddleware.ERROR_CHANNEL_NAME).(chan interface{})
-	statusChannel := r.Context().Value(wisdomMiddleware.STATUS_CHANNEL_NAME).(chan bool)
+	errorHandler := r.Context().Value(wisdomMiddleware.ERROR_CHANNEL_NAME).(chan<- interface{})
+	statusChannel := r.Context().Value(wisdomMiddleware.STATUS_CHANNEL_NAME).(<-chan bool)
 	// now publish an error to each of the wisdom errors
 	errorHandler <- errors.New("native test error")
 	// now wait for the error to be handled
