8dfcc9de7b0fc59b3b2a9acbb20987a148117e97
Jan Eike Suchard <jan@suchard.cloud>
2024-08-21 15:09:08 +0200

    removing environment parsing from tests

--- routes/basic_test.go
+++ routes/basic_test.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"context"
-	"fmt"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -12,7 +11,6 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"github.com/qustavo/dotsql"
-	wisdomType "github.com/wisdom-oss/commonTypes/v2"
 	validator "openapi.tanna.dev/go/validator/openapi3"
 
 	"microservice/globals"
@@ -23,15 +21,8 @@ var contract *openapi3.T
 func TestMain(m *testing.M) {
 	// load the variables found in the .env file into the process environment
 	godotenv.Load()
-	var c wisdomType.EnvironmentConfiguration
-	c.PopulateFromFilePath("./resources/environment.json")
-	globals.Environment, _ = c.ParseEnvironment()
-	address := fmt.Sprintf("postgres://%s:%s@%s:%s/wisdom",
-		globals.Environment["PG_USER"], globals.Environment["PG_PASS"],
-		globals.Environment["PG_HOST"], globals.Environment["PG_PORT"])
 
-	config, _ := pgxpool.ParseConfig(address)
-	globals.Db, _ = pgxpool.NewWithConfig(context.Background(), config)
+	globals.Db, _ = pgxpool.New(context.Background(), "")
 	globals.SqlQueries, _ = dotsql.LoadFromFile("./resources/queries.sql")
 
 	var err error
