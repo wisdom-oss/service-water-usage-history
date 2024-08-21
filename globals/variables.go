6d46065a22c0dd1af734c32175a89dd78beb952e
Jan Eike Suchard <jan@suchard.cloud>
2024-08-21 15:07:18 +0200

    removing environment configuration

--- globals/variables.go
+++ globals/variables.go
@@ -13,8 +13,3 @@ const ServiceName = "template" //TODO: CHANGE THIS VALUE
 
 // SqlQueries contains the prepared sql queries from the resources folder
 var SqlQueries *dotsql.DotSql
-
-// Environment contains a mapping between the environment variables and the values
-// they were set to. However, this variable only contains the configured environment
-// variables
-var Environment map[string]string = make(map[string]string)
