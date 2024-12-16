f169490965bc69d292e4e5e409f670bc5e9e5569
Jan Eike Suchard <jan@suchard.cloud>
2024-12-16 10:14:03 +0100

    adding and updating error messages

--- internal/errors/api.go
+++ internal/errors/api.go
@@ -5,8 +5,8 @@ import "github.com/wisdom-oss/common-go/v3/types"
 var ErrInvalidPageSettings = types.ServiceError{
 	Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.5.1",
 	Status: 400,
-	Title:  "Missing Consumer ID",
-	Detail: "The API could not detect any consumer id in the request",
+	Title:  "Invalid Pagination Settings",
+	Detail: "The pagination settings in your query are not supported",
 }
 
 var ErrEmptyConsumerID = types.ServiceError{
@@ -28,4 +28,32 @@ var ErrUnknownConsumer = types.ServiceError{
 	Status: 404,
 	Title:  "Unknown Consumer",
 	Detail: "No consumer with the supplied id exists",
+}
+
+var ErrEmptyARS = types.ServiceError{
+	Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.5.1",
+	Status: 400,
+	Title:  "Empty ARS",
+	Detail: "The ARS in the path is empty",
+}
+
+var ErrInvalidARS = types.ServiceError{
+	Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.5.1",
+	Status: 400,
+	Title:  "Invalid ARS",
+	Detail: "The ARS is not in a valid format",
+}
+
+var ErrEmptyUsageTypeID = types.ServiceError{
+	Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.5.1",
+	Status: 400,
+	Title:  "Empty Usage Type ID",
+	Detail: "The usage type id in the path is empty",
+}
+
+var ErrInvalidUsageTypeID = types.ServiceError{
+	Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.5.1",
+	Status: 400,
+	Title:  "Invalid Usage Type ID",
+	Detail: "The usage type id is not in a valid format",
 }
